86b5de6c5190bb81e3c356a69539ab3d57cf7b5d
Alan C <argot@digikata.com>
2021-04-30 15:50:32 -0700

    build settings
    make the jar into a "fat jar" containing all dependent
    jars too.
    
    add gradle test task run
    
    also keep old build too: gradle netjar to build

--- src/com/copper/manufacturing/PrintTestMain.java
+++ src/com/copper/manufacturing/PrintTestMain.java
@@ -6,26 +6,13 @@
 package com.copper.manufacturing;
 
 import biz.iteksolutions.IPrinterOutput;
-import biz.iteksolutions.PrinterOutputImpl;
-import biz.iteksolutions.escpos.parser.Command;
-import biz.iteksolutions.escpos.parser.DleCommand;
-import biz.iteksolutions.escpos.parser.EscposCommand;
-import biz.iteksolutions.escpos.parser.IContentOutput;
-import biz.iteksolutions.escpos.parser.InitCommand;
-import biz.iteksolutions.escpos.parser.Printer;
 import com.fazecast.jSerialComm.*;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.*;
+import javax.swing.JFrame;
 
 
 /**
@@ -35,7 +22,6 @@ import javax.swing.*;
 public class PrintTestMain {
     
     private static IPrinterOutput setupGui() {
-//        PrinterOutputImpl pPanel = new PrinterOutputImpl();
         TesterPanel pPanel = new TesterPanel();
         JFrame frame = new JFrame("Copper Cord Print Test");
         frame.setContentPane(pPanel);
@@ -57,13 +43,23 @@ public class PrintTestMain {
     }
     
     /**
+     * A "--test" arg will open the selftest-in.bin file
+     * and send it through the PrinterThread. Mostly useful for development
+     * of the tester itself.
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
         boolean do_gui = true;
         boolean do_fileio_test = false;
-        do_fileio_test = true;
+        
+        for (var arg : args) {
+            if ("--test-no-dev".equals(arg)) {
+                do_fileio_test = true;
+            }
+        }
+//        do_fileio_test = true;
         
         try {
             IPrinterOutput gui = null;
